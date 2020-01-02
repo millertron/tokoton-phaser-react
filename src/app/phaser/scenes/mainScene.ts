04e7de6dd23b2b1740570ba8bf86dbc2a8e2f1f9
Koha Choji <koha.choji@vistair.com>
2020-01-02 15:59:49 +0000

    Load player image from atlas on main scene

--- src/app/phaser/scenes/mainScene.ts
+++ src/app/phaser/scenes/mainScene.ts
@@ -1,13 +1,17 @@
 import Phaser from 'phaser'
+//@ts-ignore
+import atlas from '../assets/sprites/atlas.png'
+//@ts-ignore
+import atlasJson from '../assets/sprites/atlas.json'
 
 export class MainScene extends Phaser.Scene {
 
     preload() {
-
+        this.load.atlas('atlas', atlas, atlasJson)
     }
     
     create() {
-
+        this.add.image(100, 100, 'atlas', 'playerDefault')
     }
 
     update() {
