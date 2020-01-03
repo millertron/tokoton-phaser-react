c9518f134e5091a7e6fa1b121e48c6b1b22bab21
Koha Choji <koha.choji@vistair.com>
2020-01-03 10:28:25 +0000

    Display player controllable

--- src/app/phaser/scenes/mainScene.ts
+++ src/app/phaser/scenes/mainScene.ts
@@ -6,15 +6,45 @@ import atlasJson from '../assets/sprites/atlas.json'
 
 export class MainScene extends Phaser.Scene {
 
-    preload() {
+    private player
+    private upKey
+    private downKey
+    private leftKey
+    private rightKey
+
+    public preload() {
         this.load.atlas('atlas', atlas, atlasJson)
     }
     
-    create() {
-        this.add.image(100, 100, 'atlas', 'playerDefault')
+    public create() {
+        this.player = this.add.sprite(100, 100, 'atlas', 'playerDefault').setInteractive()
+        this.upKey = this.input.keyboard.addKey('w')
+        this.downKey = this.input.keyboard.addKey('s')
+        this.leftKey = this.input.keyboard.addKey('a')
+        this.rightKey = this.input.keyboard.addKey('d')
     }
 
-    update() {
-
+    public update() {
+        let player = this.player
+        const playerVelocity = 2
+        if (this.upKey.isDown){
+            player.y-= playerVelocity
+        }
+        if (this.downKey.isDown){
+            player.y+= playerVelocity
+        }
+        const leftKeyDown = this.leftKey.isDown
+        const rightKeyDown = this.rightKey.isDown
+        if (leftKeyDown) {
+            player.x-= playerVelocity
+            player.setFrame('playerLeft')
+        }
+        if (rightKeyDown) {
+            player.x+= playerVelocity
+            player.setFrame('playerRight')
+        }
+        if ((leftKeyDown && rightKeyDown) || (!leftKeyDown && !rightKeyDown)) {
+            player.setFrame('playerDefault')
+        }
     }
 }
