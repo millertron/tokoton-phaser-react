9075d590f2cd18ce13998c1fc34e2f9d4dfd9bdc
Koha Choji <koha.choji@vistair.com>
2020-01-17 10:21:51 +0000

    Tweak value

--- src/app/phaser/objects/missile.ts
+++ src/app/phaser/objects/missile.ts
@@ -6,6 +6,7 @@ export class Missile extends Bullet {
     private static defaultFrame = 'missile'
     static horizontalAcceleration1 = 20
     static horizontalAcceleration2 = 80
+    private static verticalAcceleration = -300
     private _horizontalAcceleration: number
 
     constructor(scene: MainScene, x: number, y: number, horizontalAcceleration: number) {
@@ -15,6 +16,6 @@ export class Missile extends Bullet {
         this._explosionFramePrefix = 'missileExplosion'
         this._explosionFrameDelayFactor = 3
         this.setVelocityY(-120)
-        this.setAcceleration(this._horizontalAcceleration, -150)
+        this.setAcceleration(this._horizontalAcceleration, Missile.verticalAcceleration)
     }
 }
