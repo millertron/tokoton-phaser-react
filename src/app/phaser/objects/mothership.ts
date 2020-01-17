4e40a56a1d2df9403d32c3aab811f2009c960727
Koha Choji <koha.choji@vistair.com>
2020-01-17 10:17:56 +0000

    Move mothership

--- src/app/phaser/objects/mothership.ts
+++ src/app/phaser/objects/mothership.ts
@@ -2,6 +2,7 @@ import { RedEnemy } from "./redEnemy"
 import { MainScene } from "../scenes/mainScene"
 import { YellowEnemy } from "./yellowEnemy"
 import { GreenEnemy } from "./greenEnemy"
+import { SCREEN_WIDTH } from "../config"
 
 export class Mothership extends Phaser.Physics.Arcade.Sprite {
     
@@ -9,14 +10,33 @@ export class Mothership extends Phaser.Physics.Arcade.Sprite {
     private static defaultFrame = 'mothershipDefault'
     private static level1Score = 100
     private static level2Score = 300
+    private static speedX = 80
+    private static speedY = 50
+    private static screenMargin = 0
+    private static amplitudeY = 180
 
     constructor(scene: MainScene, x: number, y: number) {
         super(scene, x, y, MainScene.atlasKey, Mothership.defaultFrame)
         scene.add.existing(this)
         scene.physics.add.existing(this)
+        const directionX = Math.floor(Math.random() * 2) === 0 ? -1 : 1
+        const directionY = Math.floor(Math.random() * 2) === 0 ? -1 : 1
+        this.setVelocity(directionX * Mothership.speedX, directionY * Mothership.speedY)
     }
 
     move(scene: MainScene) {
+
+        if (this.x - this.width < Mothership.screenMargin) {
+            this.setVelocityX(Mothership.speedX)
+        } else if (this.x + this.width > SCREEN_WIDTH - Mothership.screenMargin) {
+            this.setVelocityX(-Mothership.speedX)
+        }
+        if (this.y - this.height < Mothership.screenMargin) {
+            this.setVelocityY(Mothership.speedY)
+        } else if (this.y + this.height > Mothership.screenMargin + Mothership.amplitudeY){
+            this.setVelocityY(-Mothership.speedY)
+        }
+
         if (this.charge > 15) {
             const y = this.y + (this.y / 4)
             if (Math.floor(Math.random() * 1000) < 50) {
