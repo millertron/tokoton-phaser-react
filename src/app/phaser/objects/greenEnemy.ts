e1885881a38afe927d4b1f1c991bfbb31ad1c77d
millertron <senjinkoha@gmail.com>
2020-01-20 09:05:22 +0000

    Implement green enemy movement

--- src/app/phaser/objects/greenEnemy.ts
+++ src/app/phaser/objects/greenEnemy.ts
@@ -7,13 +7,22 @@ export class GreenEnemy extends EnemyProjectile {
 
     constructor(scene: MainScene, x: number, y: number) {
         super(scene, x, y, GreenEnemy.defaultFrame)
-        this.setVelocityY(100)
+        this.setVelocityY(80)
         this._hitPoints = 60
         this._scoreValue = 50
     }
 
     move(player: Player) {
-        
+        const proximityThreshold = 100
+        if (player.y - this.y > proximityThreshold) {
+            const playerFactor = player.x - this.x
+            const amplitudeFactor = 2
+            this.setVelocityX(amplitudeFactor * playerFactor)
+        } else {
+            this.setVelocityX(0)
+            this.setAccelerationY(200)
+        }
+        super.move(player)
     }
 
 }
