6cc40f5adb4f49f89c6f0f2d9a6396bb88ab382d
millertron <senjinkoha@gmail.com>
2020-01-24 11:50:43 +0000

    Fix frame changing of space torpedoes

--- src/app/phaser/objects/spaceTorpedo.ts
+++ src/app/phaser/objects/spaceTorpedo.ts
@@ -10,19 +10,16 @@ export class SpaceTorpedo extends EnemyProjectile {
     private static frameDelayFactor = 3
 
     constructor(scene: MainScene, x: number, y: number) {
-        super(scene, x, y, SpaceTorpedo.framePrefix)
+        super(scene, x, y, `${SpaceTorpedo.framePrefix}0`)
         this.setVelocityY(75)
     }
 
     move(scene: MainScene, player: Player) {
-        let frame: number = 0
-        for (let i = 1; i <= 3; i++) {
-            if (Math.floor(this._lifeTime / SpaceTorpedo.frameDelayFactor) % i === 0) {
-                frame = i - 1
-            }
-        }
-        this.setFrame(`${SpaceTorpedo.framePrefix}${frame}`)
+        this.setFrame(`${SpaceTorpedo.framePrefix}${Math.floor(this._lifeTime / SpaceTorpedo.frameDelayFactor)}`)
         this._lifeTime++
+        if (this._lifeTime > 2 * SpaceTorpedo.frameDelayFactor){
+            this._lifeTime = 0
+        }
         if (this.y > SCREEN_HEIGHT * 1.2) {
             this.destroy()
         }
