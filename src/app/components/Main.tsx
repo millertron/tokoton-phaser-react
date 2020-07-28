cc8cc17e87f5a33bd1063f9bc09624098cbae089
millertron <senjinkoha@gmail.com>
2020-07-28 10:25:28 +0100

    Adding semi-colons

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { PhaserGameContainer } from './PhaserGameContainer'
-import { Config } from '../phaser/config'
-import Phaser from 'phaser'
+import React from 'react';
+import { PhaserGameContainer } from './PhaserGameContainer';
+import { Config } from '../phaser/config';
+import Phaser from 'phaser';
 
 export const Main = () => {
     
     React.useEffect(() => {
-        const game = new Phaser.Game(Config)
-    })
+        const game = new Phaser.Game(Config);
+    });
 
     return (
         <div>
             <PhaserGameContainer />
         </div>
-    )
+    );
 }
